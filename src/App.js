2c1f801c4354d8606f19f022b4adef55fce8418a
AndrewLPimenta <pimenta.andreews@gmail.com>
2025-01-16 20:45:32 -0300

    depois de tentar bastante, dessa vez as rotas estao corretas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';  // Página principal
+import HomePage from './pages/HomePage';  // Página inicial
 import TransactionPage from './pages/TransactionPage';  // Página de gestão de gastos
 
 function App() {
   return (
-    <HomePage />
+    <Router> {/* Envolvendo todo o conteúdo com Router */}
+      <Routes> {/* Configurando as rotas */}
+        <Route path="/" element={<HomePage />} /> {/* Página inicial */}
+        <Route path="TransactionPage" element={<TransactionPage />} /> {/* Página de gestão de gastos */}
+      </Routes>
+    </Router>
   );
 }
 
